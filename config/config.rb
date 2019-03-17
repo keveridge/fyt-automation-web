f15326e7a37f9548dbb97b8a1f813d2b46319413
Kevin Thomas <kevin.thomas@tigerspike.com>
2019-03-17 12:52:01 -0400

    Add a test data class and update the test suite configuration

--- config/config.rb
+++ config/config.rb
@@ -1,9 +1,29 @@
+require 'capybara/cucumber'             # Capybara Cucumber support
+require 'rspec'                         # Framework for test expectations
+require 'pry-byebug'                    # A runtime interactive debugging tool
+
 # Define constants
 BASE_DIR = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 
 # Define the directories of variables to scan for Ruby files and load
-['step_definitions'].each do |dir|
+[
+    'step_definitions',
+    'support'
+].each do |dir|
   dir = File.join(BASE_DIR, dir)
   Dir.glob(File.join(dir, '**', '*.rb'), &method(:require))
   $LOAD_PATH.unshift(dir)
 end
+
+# Create an instance of Capybara
+Capybara.register_driver :chrome do |driver, path|
+   opts = {
+       :browser => :chrome
+   }
+   Capybara::Selenium::Driver.new(driver, opts)
+end
+
+Capybara.javascript_driver = :chrome
+Capybara.default_driver = :chrome
+
+Capybara.app_host = TestData.base_url
---
+++ support/testdata.rb
@@ -0,0 +1,14 @@
+# A class of static test data methods
+
+class TestData
+
+  # All methods in this block are static
+  class << self
+
+    def base_url
+      'http://localhost:8081'
+    end
+
+  end
+
+end
