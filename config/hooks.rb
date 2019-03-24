e1c07bfa0b52414cf160d64db6c8c15d993315da
Kevin Thomas <kevin.thomas@tigerspike.com>
2019-03-24 15:25:46 -0400

    Support for running mobile / responsive test cases

--- config/hooks.rb
+++ config/hooks.rb
@@ -2,3 +2,8 @@
 Before '@new_session' do
   Capybara.current_session.driver.quit
 end
+
+# Force the resizing of a browser if it's a mobile test case
+Before do
+  Capybara.page.driver.browser.manage.window.resize_to(488, 640) if IS_MOBILE
+end
