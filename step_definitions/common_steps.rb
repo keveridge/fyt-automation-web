a8a5c8d1dbe9197bbc5350baa827a482799bb178
Kevin Thomas <kevin.thomas@tigerspike.com>
2019-03-24 22:33:37 -0400

    Addition of product navigation feature file

--- step_definitions/common_steps.rb
+++ step_definitions/common_steps.rb
@@ -2,6 +2,7 @@
 # Feature: Main navigation -> Background
 # Feature: Footer navigation -> Background
 # Feature: Search autocomplete -> Background
+# Feature: Product navigation -> Background
 ######
 
 Given /^I'm on the (\w+) page$/ do |page|
