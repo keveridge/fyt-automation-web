5902672c27bae1be1c88f30b235a7af9fe385728
Kevin Thomas <kevin.thomas@tigerspike.com>
2019-03-29 09:40:37 -0400

    Correction to appium driver and updates to device configurations

--- config/options/drivers/appium.rb
+++ config/options/drivers/appium.rb
@@ -3,15 +3,6 @@ require 'capybara/cucumber'                   # Capybara Cucumber support
 require 'selenium/webdriver'
 require 'appium_capybara'
 
-
-desired_caps_ios = {
-    deviceName:      "iPad Pro (11-inch)",
-    platformName:    "iOS",
-    platformVersion: "12.1",
-    browserName:     "Safari",
-    orientation:     "LANDSCAPE"
-}
-
 url = "http://localhost:4723/wd/hub" # or a sauce labs url
 
 Capybara.register_driver(:appium) do |app|
@@ -20,7 +11,7 @@ Capybara.register_driver(:appium) do |app|
   }
   all_options = {
       appium_lib:  appium_lib_options,
-      caps:        desired_caps_ios
+      caps:        CONFIG['caps']
   }
   Appium::Capybara::Driver.new app, all_options
 end
